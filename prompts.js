6fc21b9f7250330becdd6d105425161def5b0ed8
211944dev <211944a@gmail.com>
2026-02-02 18:13:52 -0800

    Create prompts.js

---
+++ prompts.js
@@ -0,0 +1,10 @@
+const prompts = {
+  general: (q, grade) =>
+    `Answer clearly for a ${grade} student. Explain step by step:\n${q}`,
+
+  math: (q, grade) =>
+    `You are a math tutor. Solve step by step for a ${grade} student:\n${q}`,
+
+  english: (q, grade) =>
+    `You are an English tutor. Explain clearly for a ${grade} student:\n${q}`
+};
